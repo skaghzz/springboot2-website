6138af9cb6dc7753f901134b831573f251c77f27
namho kim <skaghzz@naver.com>
2021-07-14 22:51:00 +0900

    first commit

---
+++ src/test/java/com/skagh/springboot2website/Springboot2WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skagh.springboot2website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
