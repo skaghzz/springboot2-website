6fa5ff29ad977784bcb96296f3dbd2ce59260d83
namho kim <skaghzz@naver.com>
2021-07-18 19:11:42 +0900

    게시글 수정 기능 구현

--- src/main/java/com/skagh/springboot2website/web/IndexController.java
+++ src/main/java/com/skagh/springboot2website/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.skagh.springboot2website.web;
 
 import com.skagh.springboot2website.service.PostsService;
+import com.skagh.springboot2website.web.dto.PostsResponseDto;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,9 +20,17 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
-    
+
+    @GetMapping("/posts/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+
 }
